fce65ed7c13fd4afe105c43989e9502410ed162d
chengfu <chengfu.wcy@antfin.com>
2019-09-06 14:30:19 +0800

    ADD: digest script

---
+++ scripts/digest.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Generate template for weekly digest
+
+END=`date +"%Y%m%d"`
+DIFF=7
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    START=`date -j -f "%Y%m%d" -v-${DIFF}d ${END} +"%Y%m%d"`
+else
+    START=`date -d "${START} -${DIFF} days" "+%Y%m%d"`
+fi
+
+FILE=`date +"%Y-%m-%d"`"-weekly-digest-${END}".md
+
+cat > ${FILE} << EOF
+---
+layout: post
+title: Leetcode summary of ${START}-${END}
+categories: [leetcode]
+tags: [leetcode]
+---
+
++ [Summary](#summary)
++ [Highlight](#highlight)
+
+<a id="summary"></a>
+
+### Summary
+
+
+<a id="height"></a>
+
+### Highlight
+
+EOF
